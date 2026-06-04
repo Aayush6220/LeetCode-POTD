cf1ddc197c192cb3552abd2b2e43c9a04de8b243
Aayush6220 <aayushrastogi06@gmail.com>
2026-06-04 22:17:58 +0530

    Time: 58 ms (27.24%) | Memory: 9.6 MB (47.39%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void waviness(string nums,int &count){
         for(int i=1;i<nums.size()-1;i++){
-            if(((nums[i]-'0')>(nums[i+1]-'0') && (nums[i]-'0')>(nums[i-1]-'0')) || ((nums[i]-'0')<(nums[i+1]-'0') && (nums[i]-'0')<(nums[i-1]-'0')) ){
+            if((nums[i]>nums[i+1] && nums[i]>nums[i-1] )|| (nums[i]<nums[i+1] && nums[i]<nums[i-1]) ){
                 count++;
             }
         }
